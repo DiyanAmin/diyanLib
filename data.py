36b25caa8dcc73cfa8bd8cc3a4d8a6ac5d18987e
DiyanAmin <pakuptl3112@gamil.com>
2026-09-19 17:22:38 +0530

    Added JSON Data functions

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 #Functions for inter-file data transfer
-
+from json import load,dump,JSONDecodeError
 
 #Simple file reading.
 def store(file_path:str,data:str):
@@ -30,3 +30,37 @@ def retrieve(file_path:str):
 
     with open(file_path,'r') as f:
         return f.read()
+    
+class json_handler():
+    '''
+    Perfom data functions on json files.
+
+    :param file: The json file name.
+    :type file: str
+    '''
+    def __init__(self,file:str):
+        self.file = file
+
+    def parse(self,data:dict):
+        '''
+        Parses dictionary to json file.
+        
+        :param data: The dictionary   
+        :type data: dict
+        '''
+        with open(self.file,'w') as f:
+            dump(data,f)
+    
+    def unparse(self):
+        '''
+        Returns the data from json file as dict
+        
+        :return: Data
+        :rtype: dict
+        '''
+
+        try:
+            with open(self.file,'r') as f:
+                return load(f)
+        except (FileNotFoundError,JSONDecodeError):
+            return 'File not found'

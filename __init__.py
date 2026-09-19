2a964f79f3eca6bf6ffb295a32bbba8a67feb770
DiyanAmin <pakuptl3112@gamil.com>
2026-09-19 17:31:18 +0530

    Bug fix

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 #This file is the __init__.py file of diyanLib, created by Diyan Amin. It is available on Github.
 
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
 #Importing sub-modules
 from . import data
